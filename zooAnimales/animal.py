c6c3e421ffb4640650d147cefe08263ee54ab09b
fhurtadoa <fhurtadoa@unal.edu.co>
2024-12-16 22:05:49 -0500

    3

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -1,48 +1,67 @@
-from zooAnimales.animal import Animal
-class Anfibio(Animal):
-    _listado = []
-    ranas = 0
-    salamandras = 0
+class Animal:
+    total_animales = 0
 
-    def __init__(self, nombre=None, edad=None, habitat=None, genero=None, colorPiel=None, venenoso=None):
-        super().__init__(nombre, edad, habitat, genero)
-        self._colorPiel = colorPiel
-        self._venenoso = venenoso
-        Anfibio._listado.append(self)
+    def __init__(self, nombre=None, edad=None, habitat=None, genero=None):
+        self._nombre = nombre
+        self._edad = edad
+        self._habitat = habitat
+        self._genero = genero
+        self._zona = None
+        Animal.total_animales += 1
+
+    def getNombre(self):
+        return self._nombre
+
+    def setNombre(self, nombre):
+        self._nombre = nombre
+
+    def getEdad(self):
+        return self._edad
+
+    def setEdad(self, edad):
+        self._edad = edad
+
+    def getHabitat(self):
+        return self._habitat
+
+    def setHabitat(self, habitat):
+        self._habitat = habitat
+
+    def getGenero(self):
+        return self._genero
+
+    def setGenero(self, genero):
+        self._genero = genero
+
+    def getZona(self):
+        return self._zona
+
+    def setZona(self, zona):
+        self._zona = zona
 
     @staticmethod
-    def getListado():
-        return Anfibio._listado
+    def getTotalAnimales():
+        return Animal.total_animales
 
     @staticmethod
-    def setListado(lista):
-        Anfibio._listado = lista
-
-    def getColorPiel(self):
-        return self._colorPiel
-
-    def setColorPiel(self, colorPiel):
-        self._colorPiel = colorPiel
-
-    def isVenenoso(self):
-        return self._venenoso
-
-    def setVenenoso(self, venenoso):
-        self._venenoso = venenoso
+    def setTotalAnimales(total):
+        Animal.total_animales = total
 
     @staticmethod
-    def cantidadAnfibios():
-        return len(Anfibio._listado)
+    def totalPorTipo():
+        from zooAnimales.anfibio import Anfibio
+        from zooAnimales.ave import Ave
+        from zooAnimales.mamifero import Mamifero
+        from zooAnimales.pez import Pez
+        from zooAnimales.reptil import Reptil
+        return f"Mamiferos : {Mamifero.cantidadMamiferos()}\nAves : {Ave.cantidadAves()}\nReptiles : {Reptil.cantidadReptiles()}\nPeces : {Pez.cantidadPeces()}\nAnfibios : {Anfibio.cantidadAnfibios()}"
+    
+    def toString(self):
+        if self._zona is None:
+            return f"Mi nombre es {self._nombre}, tengo una edad de {self._edad}, habito en {self._habitat} y mi genero es {self._genero}"
+        else:
+            return f"Mi nombre es {self._nombre}, tengo una edad de {self._edad}, habito en {self._habitat} y mi genero es {self._genero}, la zona en la que me ubico es {self._zona}, en el {self._zona.zoo}"
 
     def movimiento(self):
-        return "saltar"
-
-    @staticmethod
-    def crearSalamandra(nombre, edad, genero):
-        Anfibio.salamandras += 1
-        return Anfibio(nombre, edad, "selva", genero, "negro y amarillo", False)
-
-    @staticmethod
-    def crearRana(nombre, edad, genero):
-        Anfibio.ranas += 1
-        return Anfibio(nombre, edad, "selva", genero, "rojo", True)
+        return "desplazarse"
+    
